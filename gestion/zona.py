34ea441b197c851af6003a47bcc5913be90fe0c4
Yeferson Andres Velazco Galvis <92173102+YeanVega@users.noreply.github.com>
2021-11-22 00:35:27 -0500

    Add files via upload

---
+++ gestion/zona.py
@@ -0,0 +1,18 @@
+﻿class Zona:
+    def __init__(self,nombre,zoo = None):
+        self.nombre = nombre
+        self.zoo = zoo
+        self.animales = []
+
+    def getNombre(self):
+        return self.nombre
+
+    def getZoo(self):
+        return self.zoo
+
+    def agregarAnimales(self,animal):
+        self.animales.append(animal)
+        
+    def cantidadAnimales(self):
+        resultado = self.animales
+        return len(resultado)
---
+++ zooAnimales/pez.py
@@ -0,0 +1,41 @@
+﻿class Pez(Animal):
+
+    def __init__(self, nombre, edad, habitat, genero, colorEscamas, cantidadAletas):
+        self.nombre = nombre
+        self.edad = edad
+        self.habitat = habitat
+        self.genero = genero
+        self.__colorEscamas = colorEscamas
+        self.__cantidadAletas = cantidadAletas
+        __listado.append(self)
+
+    def setColorEscamas(self, colorEscamas):
+        self.__colorEscamas = colorEscamas
+
+    def getColorEscamas(self):
+        return self.__colorEscamas
+
+    def setCantidadAleas(self, cantidadAletas):
+        self.__cantidadAletas = cantidadAletas
+
+    def getCantidadAletas(self):
+        return self.__cantidadAletas
+
+    def movimiento(self):
+        return "nadar"
+
+    @staticmethod
+    def cantidadPeces():
+        return len(__listado)
+
+    @staticmethod
+    def crearSalmon(nombre, edad, genero):
+        salmones += 1
+        salmon = Pez(nombre, edad, "oceano", genero, "rojo", 6)
+        return salmon
+
+    @staticmethod
+    def crearBacalao(nombre, edad, genero):
+        bacalaos += 1
+        bacalao = Pez(nombre, edad, "oceano", genero, "gris", 6)
+        return bacalao
